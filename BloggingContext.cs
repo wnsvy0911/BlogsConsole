df645929386b2c6c08459c151f1c56523a0af961
Junpyo Kim <jpkimkorea@gmail.com>
2022-04-05 22:38:24 -0500

    Initial Commit

---
+++ BloggingContext.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace BlogsConsole
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        public void AddBlog(Blog blog)
+        {
+            this.Blogs.Add(blog);
+            this.SaveChanges();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+            optionsBuilder.UseSqlServer(@config["BloggingContext:ConnectionString"]);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NLog.Web;
+using System.IO;
+using System.Linq;
+
+namespace BlogsConsole
+{
+    class Program
+    {
+        // create static instance of Logger
+        private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            logger.Info("Program started");
+
+            try
+            {
+
+                // Create and save a new Blog
+                Console.Write("Enter a name for a new Blog: ");
+                var name = Console.ReadLine();
+
+                var blog = new Blog { Name = name };
+
+                var db = new BloggingContext();
+                db.AddBlog(blog);
+                logger.Info("Blog added - {name}", name);
+
+                // Display all Blogs from the database
+                var query = db.Blogs.OrderBy(b => b.Name);
+
+                Console.WriteLine("All blogs in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
+
+            logger.Info("Program ended");
+        }
+    }
+}

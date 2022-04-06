827a90619efc46a787d85a41d455695af1875f4e
Junpyo Kim <jpkimkorea@gmail.com>
2022-04-05 23:14:45 -0500

    Present the user with 4  options: 1. Display all blogs 2. Add Blog 3. Create Post 4. Display Post Enter q to quit

--- Program.cs
+++ Program.cs
@@ -3,44 +3,97 @@ using NLog.Web;
 using System.IO;
 using System.Linq;
 
+
 namespace BlogsConsole
 {
     class Program
-    {
-        // create static instance of Logger
+    // create static instance of Logger
+    {    
         private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
+    
+
+        static string mainMenu() {
+            Console.Write("Enter your selection:");
+            Console.Write("1) Display all blogs");
+            Console.Write("2) Add Blog");
+            Console.Write("3) Create Post");
+            Console.Write("4) Display Posts");
+            Console.Write("Enter q to quit");
+
+            return Console.ReadLine();
+        }
+
+        static Blog createBlogWorkflow() {
+            Console.Write("Enter a name for a new Blog: ");
+            var name = Console.ReadLine();
+            logger.Info("Blog added - {name}", name);
+            return new Blog { Name = name };
+        }
+
+        static Post createPostWorkflow(BloggingContext db) {
+
+            Console.WriteLine("Please Choose A Blog To Post To.");
+            displayAllBlogs(db, true);
+            var blogChoice = Console.ReadLine();
+
+            Console.Write("Enter a title for the new Post:");
+            var title = Console.ReadLine();
+            Console.Write("Enter content for the new Post");
+            var content = Console.ReadLine();
+            return new Post { Title = title, Content = content };
+        }
+
+        static void displayAllBlogs(BloggingContext db, bool withId) {
+            var query = db.Blogs.OrderBy(b => b.Name);
+
+            Console.WriteLine("All blogs in the database:");
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
         static void Main(string[] args)
         {
             logger.Info("Program started");
 
             try
             {
-
-                // Create and save a new Blog
-                Console.Write("Enter a name for a new Blog: ");
-                var name = Console.ReadLine();
-
-                var blog = new Blog { Name = name };
-
                 var db = new BloggingContext();
-                db.AddBlog(blog);
-                logger.Info("Blog added - {name}", name);
-
-                // Display all Blogs from the database
-                var query = db.Blogs.OrderBy(b => b.Name);
-
-                Console.WriteLine("All blogs in the database:");
-                foreach (var item in query)
+                string choice = mainMenu();
+                switch (choice)
                 {
-                    Console.WriteLine(item.Name);
+                    case "1":
+                    // Display All Blogs, pass in database context
+                        displayAllBlogs(db, false);
+                        displayAllBlogs(db, true);
+                        break;
+                    case "2":
+                    // Add Blog
+                        db.AddBlog(createBlogWorkflow());
+                        break;
+                    case "3":
+                    // Create Post
+                        break;
+                    case "4":
+                    // Display Posts
+                        break;
+                    case "q":
+                    // Exit 
+                        break;
+                    default:
+                        Console.WriteLine("Please Enter A Valid Option.");
+                        break;
                 }
+
             }
+            
             catch (Exception ex)
             {
                 logger.Error(ex.Message);
             }
 
             logger.Info("Program ended");
+        
         }
     }
 }
